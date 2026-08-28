d5f14f7a906ac321bad8dda6af9dff9f6ea68980
thegreengu <thegreencreator32@gmail.com>
2026-08-27 22:44:54 -0400

    removed movement controls with arrow keys to get better at hjkl

--- lua/custom/keybinds.lua
+++ lua/custom/keybinds.lua
@@ -8,8 +8,8 @@ vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[L
 vim.keymap.set({ "i", "n" }, "<leader>rf", vim.lsp.buf.format, { desc = "[LSP] Reformat code" .. disclaimer })
 
 
--- QUIZAS ES MALA IDEA???????
--- vim.keymap.set("n", "h", "<C-w>h", { desc = "Window left" })
--- vim.keymap.set("n", "j", "<C-w>j", { desc = "Window down" })
--- vim.keymap.set("n", "k", "<C-w>k", { desc = "Window up" })
--- vim.keymap.set("n", "l", "<C-w>l", { desc = "Window right" })
+vim.keymap.set({"n", "v"}, "<Up>", "<Nop>")
+vim.keymap.set({"n", "v"}, "<Down>", "<Nop>")
+vim.keymap.set({"n", "v"}, "<Left>", "<Nop>")
+vim.keymap.set({"n", "v"}, "<Right>", "<Nop>")
+
